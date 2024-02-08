93f706823a1dd722dc1cd71fddbf1074e06b98d8
lleccar02 <luisrleccar@proton.me>
2024-02-08 15:54:10 -0500

    Migrate NotesList to functional component

--- README.md
+++ README.md
@@ -3,8 +3,8 @@
 Ejercicio de React siguiente los videos de [Stack MERN](https://www.youtube.com/playlist?list=PLo5lAe9kQrwrGPjhhzejCt3JENYf5uDNf) de Fazt Code.
 
 ## TODO
-- [ ] Convertir los componentes de clase a componentes funcionales
+- [x] Convertir los componentes de clase a componentes funcionales
     -  [x] CreateNote.js
     -  [x] CreateUser.js
     -  [x] Navigation.js
-    -  [ ] NotesList.js
+    -  [x] NotesList.js
--- frontend/src/components/NotesList.jsx
+++ frontend/src/components/NotesList.jsx
@@ -1,56 +1,56 @@
-import { Component } from 'react'
-import axios from 'axios'
+import { useState, useEffect } from 'react';
+import axios from 'axios';
 import { Link } from 'react-router-dom';
-import TimeAgo from 'react-timeago'
+import TimeAgo from 'react-timeago';
 
-export default class NotesList extends Component {
+const NotesList = () => {
+  const [notes, setNotes] = useState([]);
 
-  state = {
-    notes: []
-  }
+  useEffect(() => {
+    getNotes();
+  }, []);
 
-  componentDidMount() {
-    this.getNotes();
-  }
-
-  getNotes = async () => {
+  const getNotes = async () => {
     const res = await axios.get('http://localhost:4000/api/notes');
-    this.setState({notes: res.data.notes});
-  }
+    setNotes(res.data.notes);
+  };
 
-  deleteNote = async (id) => {
-    await axios.delete('http://localhost:4000/api/notes/' + id);
-    this.getNotes();
-  }
+  const deleteNote = async (id) => {
+    await axios.delete(`http://localhost:4000/api/notes/${id}`);
+    getNotes();
+  };
 
-  render() {
-    return (
-      <div className="row">
-        {
-          this.state.notes.map(note => (
-            <div className="col-md-4 p-2" key={note._id}>
-              <div className="card">
-                <div className="card-header d-flex justify-content-between">
-                  <h5>{note.title}</h5>
-                  <Link className="nav-link" to={"/edit/" + note._id}>Edit</Link>
-                </div>
-                <div className="card-body">
-                  <p>{note.content}</p>
-                  <p>{note.author}</p>
-                  <p>
-                    <TimeAgo date={note.date} />
-                  </p>
-                </div>
-                <div className="card-footer">
-                  <button className="btn btn-danger" onClick={() => this.deleteNote(note._id)}>
-                    Delete
-                  </button>
-                </div>
-              </div>
+  return (
+    <div className="row">
+      {notes.map(note => (
+        <div className="col-md-4 p-2" key={note._id}>
+          <div className="card">
+            <div className="card-header d-flex justify-content-between">
+              <h5>{note.title}</h5>
+              <Link className="nav-link" to={`/edit/${note._id}`}>
+                Edit
+              </Link>
             </div>
-          ))
-        }
-      </div>
-    )
-  }
-}
+            <div className="card-body">
+              <p>{note.content}</p>
+              <p>{note.author}</p>
+              <p>
+                <TimeAgo date={note.date} />
+              </p>
+            </div>
+            <div className="card-footer">
+              <button
+                className="btn btn-danger"
+                onClick={() => deleteNote(note._id)}
+              >
+                Delete
+              </button>
+            </div>
+          </div>
+        </div>
+      ))}
+    </div>
+  );
+};
+
+export default NotesList;

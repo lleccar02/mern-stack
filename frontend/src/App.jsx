be7a001fb4af0d6ce12b8e3910cf5959098277c1
lleccar02 <luisrleccar@proton.me>
2024-02-08 16:09:40 -0500

    Import bootstrap js

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,6 @@
 import { Route, Routes } from 'react-router-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.bundle.min';
 import './App.css'
 
 import Navigation from './components/Navigation';

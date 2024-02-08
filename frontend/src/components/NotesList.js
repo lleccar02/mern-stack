7f69726910fe9806f6f250202678b34b9beb70de
lleccar02 <luisrleccar@proton.me>
2024-02-08 11:26:41 -0500

    Listar y eliminar notas

--- frontend/src/components/NotesList.js
+++ frontend/src/components/NotesList.js
@@ -1,9 +1,53 @@
 import React, { Component } from 'react'
+import axios from 'axios'
+import { format } from 'timeago.js'
 
 export default class NotesList extends Component {
+
+  state = {
+    notes: []
+  }
+
+  componentDidMount() {
+    this.getNotes();
+  }
+
+  getNotes = async () => {
+    const res = await axios.get('http://localhost:4000/api/notes');
+    this.setState({notes: res.data.notes});
+  }
+
+  deleteNote = async (id) => {
+    await axios.delete('http://localhost:4000/api/notes/' + id);
+    this.getNotes();
+  }
+
   render() {
     return (
-      <div>NotesList</div>
+      <div className="row">
+        {
+          this.state.notes.map(note => (
+            <div className="col-md-4 p-2" key={note._id}>
+              <div className="card">
+                <div className="card-header d-flex justify-content-between">
+                  <h5>{note.title}</h5>
+                  <a href={'/edit/' + note._id}>Edit</a>
+                </div>
+                <div className="card-body">
+                  <p>{note.content}</p>
+                  <p>{note.author}</p>
+                  <p>{format(note.date)}</p>
+                </div>
+                <div className="card-footer">
+                  <button className="btn btn-danger" onClick={() => this.deleteNote(note._id)}>
+                    Delete
+                  </button>
+                </div>
+              </div>
+            </div>
+          ))
+        }
+      </div>
     )
   }
 }

a6b6c584b44e56166355c22ed4cec6a49da31511
lleccar02 <luisrleccar@proton.me>
2024-02-08 10:35:23 -0500

    Agregar y eliminar usuario

--- frontend/src/components/CreateUser.js
+++ frontend/src/components/CreateUser.js
@@ -9,6 +9,10 @@ export default class CreateUser extends Component {
   }
 
   async componentDidMount() {
+    this.getUsers();
+  }
+
+  getUsers = async () => {
     const res = await axios.get('http://localhost:4000/api/users');
     this.setState({users: res.data.users});
   }
@@ -19,19 +23,36 @@ export default class CreateUser extends Component {
     })
   }
 
+  onSubmit = async (e) => {
+    e.preventDefault();
+    await axios.post('http://localhost:4000/api/users', {
+      username: this.state.username
+    });
+    this.setState({username: ''});
+    this.getUsers();
+  }
+
+  deleteUser = async (id) => {
+    await axios.delete('http://localhost:4000/api/users/' + id);
+    this.getUsers();
+  }
+
   render() {
     return (
       <div className='row'>
         <div className="col-md-4">
           <div className="card card-body">
             <h3>Create New User</h3>
-            <form action="">
+            <form onSubmit={this.onSubmit}>
               <div className="form-group">
                 <input 
+                  value={this.state.username}
                   type="text" 
                   className="form-control" 
                   onChange={this.onChangeUsername}
                 />
+                <br />
+                <button type="submit" className="btn btn-primary">Save</button>
               </div>
             </form>
           </div>
@@ -40,7 +61,11 @@ export default class CreateUser extends Component {
             <ul className="list-group">
               {
                 this.state.users.map(user => (
-                  <li className="list-group-item list-group-item-action" key={user._id}>
+                  <li 
+                    className="list-group-item list-group-item-action" 
+                    key={user._id}
+                    onDoubleClick={() => this.deleteUser(user._id)}
+                  >
                     {user.username}
                   </li>
                 ))
